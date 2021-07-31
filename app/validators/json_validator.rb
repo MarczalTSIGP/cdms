27e7e20524eda64d284bb14de67b5dd50fce1a79
lamezon <matheuslamezon@gmail.com>
2021-07-30 22:51:28 -0300

    Validator com Regex

--- app/validators/json_validator.rb
+++ app/validators/json_validator.rb
@@ -1,12 +1,21 @@
 class JsonValidator < ActiveModel::EachValidator
+    REGEX_NAME = /[{}@!#%^&*()$]/
+    REGEX_IDENTIFIER = /[{}@!#%^&*_()$\s\[\]]/
+    
     def validate_each(record, attribute, value)
         unless value.is_a?(Array)
             record.errors.add attribute, (options[:message] || "is not an Array")
-        end
+        end  
         if value.is_a?(Array)
             value.each do |json|
                 unless json.has_key?('name') && json.has_key?('identifier')
                     record.errors.add attribute, (options[:message] || "element #{json} with invalid format")
+                end
+                if json['name'] =~ REGEX_NAME
+                    record.errors[attribute] << (options[:message] || "element #{json} with invalid name")
+                end
+                if json['identifier'] =~ REGEX_IDENTIFIER
+                    record.errors[attribute] << (options[:message] || "element #{json} with invalid identifier")
                 end
             end
         end
