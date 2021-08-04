f7190e329fd5e91d889061f242fef03281b14bb0
cristhianalbary <cristhian.albary@envolvelabs.com.br>
2021-08-04 02:28:23 -0300

    remove offenses on rubocop

--- app/validators/json_validator.rb
+++ app/validators/json_validator.rb
@@ -1,31 +1,43 @@
 class JsonValidator < ActiveModel::EachValidator
-  REGEX_NAME = /[{}@!#%^&*()$]/
-  REGEX_IDENTIFIER = /[{}@!#%^&*_()$\s]/
+  REGEX_NAME << /[{}@!#%^&*()$]/
+  REGEX_IDENTIFIER << /[{}@!#%^&*_()$\s]/
 
   def validate_each(record, attribute, value)
-    unless value.is_a?(Array)
-      record.errors.add attribute, (options[:message] || 'is not an Array')
-    end
-    if value.is_a?(Array)
-      value.each do |json|
-        unless json.key?('name') && json.key?('identifier')
-          record.errors.add attribute, (options[:message] || "element #{json} with invalid format")
-        end
-        if json['name'] =~ REGEX_NAME
-          record.errors[attribute] << (options[:message] || "element #{json} with invalid name")
-        end
-        if json['identifier'] =~ REGEX_IDENTIFIER
-          record.errors[attribute] << (options[:message] || "element #{json} with invalid identifier")
-        end
-        #ambiguous_value(record, json['identifier'], value)
-      end
-    end
+    return unless value.is_a?(Array)
+
+    record.errors.add attribute, (options[:message] || 'is not an Array')
   end
 
   def ambiguous_value(record, identifier, value)
     value.each do |json|
       unless identifier == json['identifier']
         record.errors.add(:identifier, I18n.t('errors.messages.taken'))
+      end
+    end
+  end
+
+  def validate_json(record, attribute, value)
+    if value.is_a?(Array)
+      value.each do |json|
+        unless json.key?('name') && json.key?('identifier')
+          record.errors.add attribute, (options[:message] || "element #{json} with invalid format")
+        end
+      end
+    end
+  end
+
+  def validate_name(record, attribute, value)
+    value.each do |json|
+      if json['name'] =~ REGEX_NAME
+        record.errors[attribute] << (options[:message] || "element #{json} with invalid name")
+      end
+    end
+  end
+
+  def validate_identifier(record, attribute, value)
+    value.each do |json|
+      if json['identifier'] =~ REGEX_IDENTIFIER
+        record.errors[attribute] << (options[:message] || "element #{json} with invalid identifier")
       end
     end
   end
