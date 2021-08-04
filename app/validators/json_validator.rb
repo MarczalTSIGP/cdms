43674fa1bd862a1b33d1be6d2feefb6841dbdb5f
cristhianalbary <cristhian.albary@envolvelabs.com.br>
2021-08-04 02:41:46 -0300

    remove offenses on rubocop

--- app/validators/json_validator.rb
+++ app/validators/json_validator.rb
@@ -3,25 +3,19 @@ class JsonValidator < ActiveModel::EachValidator
   REGEX_IDENTIFIER << /[{}@!#%^&*_()$\s]/
 
   def validate_each(record, attribute, value)
-    return unless value.is_a?(Array)
-
-    record.errors.add attribute, (options[:message] || 'is not an Array')
+    return record.errors.add attribute, (options[:message] || 'is not an Array') unless value.is_a?(Array)
   end
 
   def ambiguous_value(record, identifier, value)
     value.each do |json|
-      unless identifier == json['identifier']
-        record.errors.add(:identifier, I18n.t('errors.messages.taken'))
-      end
+      return record.errors.add(:identifier, I18n.t('errors.messages.taken')) unless identifier == json['identifier']
     end
   end
 
   def validate_json(record, attribute, value)
-    if value.is_a?(Array)
-      value.each do |json|
-        unless json.key?('name') && json.key?('identifier')
-          record.errors.add attribute, (options[:message] || "element #{json} with invalid format")
-        end
+    value.each do |json|
+      unless json.key?('name') && json.key?('identifier')
+        record.errors.add attribute, (options[:message] || "element #{json} with invalid format")
       end
     end
   end
