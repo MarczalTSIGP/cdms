66774b198aa891fb96b279f0c7be4247a6f5231b
cristhianalbary <cristhian.albary@envolvelabs.com.br>
2021-08-25 14:15:44 -0300

    add user forms for add member to document

--- app/controllers/users/document_member_controller.rb
+++ app/controllers/users/document_member_controller.rb
@@ -1,5 +1,12 @@
 class Users::DocumentMemberController < Users::BaseController
 
-    def index; end
+    def index
+        @document = current_user.documents.find(params[:documentId])
+        if params[:memberId].to_i >= 1
+            @user = User.find(params[:memberId])
+        else
+            @user = User.new
+        end
+    end
 
 end
