dcabbcaaaa61e1d78f79fc4e5134e4312086738a
Diego Marczal <dmarczal@gmail.com>
2021-10-12 22:34:54 -0300

    Addded number of departments to admin dashboard

--- test/system/admins/dashboard/dashboard_test.rb
+++ test/system/admins/dashboard/dashboard_test.rb
@@ -22,5 +22,24 @@ class DashboardTest < ApplicationSystemTestCase
       assert_selector '.list-group-item', text: I18n.t('views.app.sidebar.users')
       assert_selector '.list-group-item', text: I18n.t('views.app.sidebar.administrators')
     end
+
+    should 'display the number of departments in singular mode' do
+      create(:department)
+      visit admins_root_path
+
+      selector = 'div.row.row-cards div.card:first-child .card-body'
+
+      assert_selector selector, text: 1
+      assert_selector selector, text: Department.model_name.human(count: 1)
+    end
+
+    should 'display the number of departments in plural mode' do
+      create_list(:department, 3)
+      visit admins_root_path
+
+      selector = 'div.row.row-cards div.card:first-child .card-body'
+      assert_selector selector, text: 3
+      assert_selector selector, text: Department.model_name.human(count: 3)
+    end
   end
 end
