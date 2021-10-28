e54861bf3d7d0547fd52564dc4467b4150fd9347
André de Souza Aranda <andredesouzaaranda@gmail.com>
2021-10-28 13:04:17 -0300

    Added cards to admin dashboard (corrections)

--- test/models/admin_dashboard_test.rb
+++ test/models/admin_dashboard_test.rb
@@ -21,55 +21,58 @@ class AdminDashboarTest < ActiveSupport::TestCase
   end
 
   context 'active users' do
-    should 'return number of active users' do
-      assert_equal 0, AdminDashboard.active_users.count
+    setup do
       create(:user, active: true)
+    end
+
+    should 'return number of active users' do
       assert_equal 1, AdminDashboard.active_users.count
     end
 
     should 'return translation of active users in singular' do
-      create(:user, active: true)
       assert_equal User.model_name.human(count: 1), AdminDashboard.active_users.human
     end
 
     should 'return translation of active users in plural' do
-      create_list(:user, 2, active: true)
+      create(:user, active: true)
       assert_equal User.model_name.human(count: 2), AdminDashboard.active_users.human
     end
   end
 
   context 'inactive users' do
-    should 'return number of inactive users' do
-      assert_equal 0, AdminDashboard.inactive_users.count
+    setup do
       create(:user, active: false)
+    end
+
+    should 'return number of inactive users' do
       assert_equal 1, AdminDashboard.inactive_users.count
     end
 
     should 'return translation of inactive users in singular' do
-      create(:user, active: false)
       assert_equal User.model_name.human(count: 1), AdminDashboard.inactive_users.human
     end
 
     should 'return translation of inactive users in plural' do
-      create_list(:user, 2, active: false)
+      create(:user, active: false)
       assert_equal User.model_name.human(count: 2), AdminDashboard.inactive_users.human
     end
   end
 
   context 'audience members' do
-    should 'return number of audience members' do
-      assert_equal 0, AdminDashboard.audience_members.count
+    setup do
       create(:audience_member)
+    end
+
+    should 'return number of audience members' do
       assert_equal 1, AdminDashboard.audience_members.count
     end
 
     should 'return translation of audience members in singular' do
-      create(:audience_member)
       assert_equal AudienceMember.model_name.human(count: 1), AdminDashboard.audience_members.human
     end
 
     should 'return translation of audience members in plural' do
-      create_list(:audience_member, 2)
+      create(:audience_member)
       assert_equal AudienceMember.model_name.human(count: 2), AdminDashboard.audience_members.human
     end
   end
