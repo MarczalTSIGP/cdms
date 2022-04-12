f6ef9d2fb12303442b07a87dea059cd41fbaff60
Beatriz <beatrizalmeida354@gmail.com>
2022-04-12 02:46:42 -0300

    Changed searchable to case insensitive by default

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -7,13 +7,23 @@ module Searchable
     private
 
     def search_by(*fields)
-      @fields = fields
+      @fields = []
+      fields.each do |field|
+        field.class == Symbol ? @fields.push({ field => { case_sensitive: false } }) : @fields.push(field)
+      end
     end
   end
 
   included do
     def self.search(term)
-      where(@fields.map { |field| "#{field} LIKE :term" }.join(' OR '), term: "%#{term}%")
+      where(@fields.map { |field| compare(field, term) }.join(' OR '))
     end
   end
+end
+
+private
+def compare(field, term)
+  key = field.keys[0]
+  operator = field[key][:case_sensitive] ? 'LIKE' : 'ILIKE'
+  "#{key} #{operator} '%#{term}%'"
 end
