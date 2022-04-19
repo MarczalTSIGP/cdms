4bd8ffe03f2ffda6b14df580a18569af77b7eef8
Diego Marczal <dmarczal@gmail.com>
2022-04-19 20:24:00 -0300

    Review and refactored PR bruw-refactor-api-to-be-generic

--- app/controllers/api/search_members_controller.rb
+++ app/controllers/api/search_members_controller.rb
@@ -1,9 +1,17 @@
 class Api::SearchMembersController < ActionController::API
   def search_non_members
-    model_name = params[:model_name]
-    @model = model_name.classify.constantize.find(params[:id])
+    @model = model.find(params[:id])
     non_members = @model.search_non_members(params[:term])
 
     render json: non_members.as_json(only: [:id, :name])
+  end
+
+  private
+
+  def model
+    model_name = params[:model_name].classify
+
+    whitelist = [::Department, ::DepartmentModule]
+    whitelist.find { |model| model.name.eql?(model_name) }
   end
 end
