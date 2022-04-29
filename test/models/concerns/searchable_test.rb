a4073f0e387bc506fb3b24cfb89af2e8bddf0f76
Beatriz Fernandes <43864137+BeaFernandes@users.noreply.github.com>
2022-04-29 01:46:21 -0300

    Changed searchable to case insensitive by default (#144)
    * Changed searchable to case insensitive by default
    
    * Refactored Searchable options
    
    Co-authored-by: Diego Marczal <dmarczal@gmail.com>

---
+++ test/models/concerns/searchable_test.rb
@@ -0,0 +1,62 @@
+require 'test_helper'
+
+class SearchableTest < ActiveSupport::TestCase
+  setup do
+    @term = 'search-term'
+    @mock = Minitest::Mock.new
+  end
+
+  should 'build case insensitive condition by default' do
+    @mock.expect :call, nil, ['name ILIKE :term', { term: "%#{@term}%" }]
+
+    SearchCaseInsensitive.stub(:where, @mock) do
+      SearchCaseInsensitive.search(@term)
+    end
+
+    assert_mock @mock
+  end
+
+  should 'build case sensitive condition' do
+    @mock.expect :call, nil, ['name LIKE :term', { term: "%#{@term}%" }]
+
+    SearchCaseSensitive.stub(:where, @mock) do
+      SearchCaseSensitive.search(@term)
+    end
+
+    assert_mock @mock
+  end
+
+  should 'build condition with two params' do
+    @mock.expect :call, nil, ['name ILIKE :term OR initials LIKE :term', { term: "%#{@term}%" }]
+
+    SearchWithParams.stub(:where, @mock) do
+      SearchWithParams.search(@term)
+    end
+
+    assert_mock @mock
+  end
+end
+
+class SearchCaseInsensitive
+  include Searchable
+
+  search_by :name
+
+  def self.where(*args); end
+end
+
+class SearchCaseSensitive
+  include Searchable
+
+  search_by name: { case_sensitive: true }
+
+  def self.where(*args); end
+end
+
+class SearchWithParams
+  include Searchable
+
+  search_by :name, initials: { case_sensitive: true }
+
+  def self.where(*args); end
+end
