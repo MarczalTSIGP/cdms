61c8428398e5093f290f8e6473d54f8d11cf8210
rgoudinho <ronaldogoudinho815@gmail.com>
2022-09-21 10:44:51 -0300

    corrections in sending an email to notify that the user has a document to sign

--- app/mailers/notify_document_to_sign_mailer.rb
+++ app/mailers/notify_document_to_sign_mailer.rb
@@ -1,10 +1,7 @@
 class NotifyDocumentToSignMailer < ApplicationMailer
     def notify_sign
         @document = params[:document]
-        @recipient = User.find_by(cpf: params[:cpf])  
-        if !@recipient 
-            @recipient = AudienceMember.find_by(cpf: params[:cpf])
-        end        
-        mail(to: @recipient.email, subject: t('devise.mailer.notify_sign.action'))
+        @signer = User.find(params[:user_id])  
+        mail(to: @signer.email, subject: t('devise.mailer.notify_sign.action'))
     end
 end
