a081f10438e81c2ba1bcfdd94780c96f24852508
rgoudinho <ronaldogoudinho815@gmail.com>
2022-09-21 16:07:51 -0300

    adding test for notify_sign and creating notify_sign_text

--- test/mailers/notify_document_to_sign_mailer_test.rb
+++ test/mailers/notify_document_to_sign_mailer_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class NotifyDocumentToSignMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup 
+    department = create(:department)
+    @signer = create(:user)
+    @document = create(:document, :declaration, department: department)
+  end
+  
+  test "notify_sign" do
+    email = NotifyDocumentToSignMailer.with(user_id: @signer.id, document: @document).notify_sign
+    assert_emails 1 do 
+      email.deliver_later
+    end
+
+    assert_equal email.to, [@signer.email]
+    assert_equal email.from, ['sgcd@utfpr.edu.br']
+    assert_equal email.subject, 'Documento para assinar'
+    assert_match "Olá #{@signer.name}!\n\nVocê tem um novo documento para assinar\n\nIr para o documento", email.body.encoded
+  end
 end
