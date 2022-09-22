49467eec859ea177476317fe42e9f4a38715c9ad
rgoudinho <ronaldogoudinho815@gmail.com>
2022-09-22 16:54:43 -0300

    some quality fixes

--- app/mailers/notify_document_to_sign_mailer.rb
+++ app/mailers/notify_document_to_sign_mailer.rb
@@ -1,7 +1,7 @@
 class NotifyDocumentToSignMailer < ApplicationMailer
-    def notify_sign
-        @document = params[:document]
-        @signer = User.find(params[:user_id])  
-        mail(to: @signer.email, subject: t('devise.mailer.notify_sign.action'))
-    end
+  def notify_sign
+	  @document = params[:document]
+    @signer = User.find(params[:user_id])
+    mail(to: @signer.email, subject: t('devise.mailer.notify_sign.action'))
+  end
 end
--- test/mailers/notify_document_to_sign_mailer_test.rb
+++ test/mailers/notify_document_to_sign_mailer_test.rb
@@ -1,20 +1,20 @@
 require 'test_helper'
 
 class NotifyDocumentToSignMailerTest < ActionMailer::TestCase
-  def setup 
+  def setup
     @signer = create(:user)
     @document = create(:document, :declaration)
   end
   
-  test "notify_sign" do
+  test 'notify_sign' do
     email = NotifyDocumentToSignMailer.with(user_id: @signer.id, document: @document).notify_sign
-    assert_emails 1 do 
+    assert_emails 1 do
       email.deliver_later
     end
 
     assert_equal email.to, [@signer.email]
     assert_equal email.from, ['sgcd@utfpr.edu.br']
     assert_equal email.subject, 'Documento para assinar'
-    assert_match "tem um novo documento para assinar", email.body.encoded
+    assert_match 'tem um novo documento para assinar', email.body.encoded
   end
 end
