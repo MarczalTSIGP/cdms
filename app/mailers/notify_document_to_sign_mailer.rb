b92c7ad05e4d1271bbbacebcb98f9bda5a773da9
rgoudinho <ronaldogoudinho815@gmail.com>
2022-09-22 17:27:38 -0300

    some quality fixes

--- app/mailers/notify_document_to_sign_mailer.rb
+++ app/mailers/notify_document_to_sign_mailer.rb
@@ -1,6 +1,6 @@
 class NotifyDocumentToSignMailer < ApplicationMailer
   def notify_sign
-	  @document = params[:document]
+    @document = params[:document]
     @signer = User.find(params[:user_id])
     mail(to: @signer.email, subject: t('devise.mailer.notify_sign.action'))
   end
