638bcb09dfb7b6652da158e45b4ac9ce419fd103
TaisHryssai <thryssai@gmail.com>
2022-09-22 18:03:16 -0300

    save data after signing the document

--- app/models/document_signer.rb
+++ app/models/document_signer.rb
@@ -4,4 +4,15 @@ class DocumentSigner < ApplicationRecord
   belongs_to :document_role
 
   validates :user, uniqueness: { scope: :document_id }
+
+  def signed_documents
+    DocumentSigner.where(signed: true)
+  end
+
+  def sign(role)
+    date = Time.current
+    DocumentSigner.includes(:document, :user, :document_role).update(
+      signed: true, date_hour: date, role_user_signed: role
+    )
+  end
 end
