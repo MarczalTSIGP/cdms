8613954f933eb87ac05e336cebc17586a49b4f82
rgoudinho <ronaldogoudinho815@gmail.com>
2022-11-02 22:49:34 -0300

    added Logics::Document::Signer and test class for it

--- app/services/send_emails.rb
+++ app/services/send_emails.rb
@@ -1,34 +1,21 @@
 class SendEmails
 
 	def initialize(params={})
-    @signers
+    @signers = Logics::Document::Signer.new
 	end
 	
   def perform
-	  search_for_recipients
     send
   end
 
 	private
 
   def send
-		@signers.each do |document_id, document_signers|
-      @emails = [1,2,3].map {|v| v*2}
-      document_signers.each do |signer|
-        add_email(user_id: signer.user_id)
-      end
+		@signers.search_for_recipients.each do |document_id, document_signers|
+      @emails = @signers.get_emails(document_signers: document_signers)
       send_email(document_id: document_id)
 		end
 	end
-
-	def search_for_recipients
-    @signers = DocumentSigner.where(signed: false).group_by(&:document_id)
-	end 
-
-  def add_email(params = {})
-    user = User.find(params[:user_id])
-    @emails.unshift(user.email) 
-  end
 
   def send_email(params = {})
     NotifyDocumentToSignMailer.with(emails: @emails , document_id: params[:document_id]).notify_sign.deliver_now
