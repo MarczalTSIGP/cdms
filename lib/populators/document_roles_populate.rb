f2289bbd95cb2e6bcef66d12306ccb186d0344b8
Lucas Fernando <lucasf.geron@icloud.com>
2023-03-17 16:52:57 -0300

    code quality: fix rubocop offenses update

--- lib/populators/document_roles_populate.rb
+++ lib/populators/document_roles_populate.rb
@@ -1,35 +1,37 @@
 require_relative 'base_populate'
 
 class Populators::DocumentRolesPopulate < Populators::BasePopulate
-  
+  attr_accessor :document_roles
+
   def self.populate(amount = 0)
     instance.delete
-    amount.zero? ? self.create : amount.times { self.create_with_faker }
+    amount.zero? ? instance.create : amount.times { instance.create_with_faker }
     instance.log(amount.zero? ? DocumentRole.count : amount)
   end
 
-  def self.create
-    document_roles = [
+  def initialize(*)
+    super
+    self.document_roles = [
       { name: 'Aluno', description: 'Acadêmico da Instituição' },
-      { name: 'Professor', description: 'Professor da Instituição' },
-      { name: 'Coordenador', description: 'Coordenador do Curso' },
-      { name: 'Diretor', description: 'Diretor de Câmpus' },
-      { name: 'Secretário', description: 'Secretário Geral' }
+      { name: 'Professor', description: 'Professor da Disciplina' },
+      { name: 'Coordenador', description: 'Coordenador do Curso' }
     ]
+  end
 
+  def create
     document_roles.each do |document_role|
       DocumentRole.create!(
-      name: document_role[:name],
-      description: document_role[:description]
-    )
+        name: document_role[:name],
+        description: document_role[:description]
+      )
     end
   end
-  
-#   USED TO CREATE FAKER DOCUMENT ROLES
-  def self.create_with_faker
+
+  #   USED TO CREATE FAKER DOCUMENT ROLES
+  def create_with_faker
     DocumentRole.create!(
-    name: Faker::Job.position,
-    description: Faker::Job.title
-  )
+      name: Faker::Job.position,
+      description: Faker::Job.title
+    )
   end
 end
