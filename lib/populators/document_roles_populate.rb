7e4a3147acd644b00860d3bc1c8936432a4c30b4
Lucas Fernando <lucasf.geron@icloud.com>
2023-03-17 16:08:50 -0300

    code quality review

--- lib/populators/document_roles_populate.rb
+++ lib/populators/document_roles_populate.rb
@@ -2,35 +2,31 @@ require_relative 'base_populate'
 
 class Populators::DocumentRolesPopulate < Populators::BasePopulate
   
-    def self.populate(_amount = 0)
-        instance.delete
-        _amount > 0 ? _amount.times { self.create_with_faker } : self.create
-        instance.log(_amount > 0 ? _amount : DocumentRole.count)
-    end
+  def self.populate(amount = 0)
+    instance.delete
+    amount.zero? ? self.create : amount.times { self.create_with_faker }
+    instance.log(amount.zero ? amount : DocumentRole.count)
+  end
     
-    def self.create
-        document_roles = [
-            {name: 'Aluno', description: 'Acadêmico da Instituição'}, 
-            {name: 'Professor', description: 'Professor da Instituição'}, 
-            {name: 'Coordenador', description: 'Coordenador do Curso'}, 
-            {name: 'Diretor', description: 'Diretor de Câmpus'}, 
-            {name: 'Secretário', description: 'Secretário Geral'}
-        ]
+def self.create
+  document_roles = [
+    { name: 'Aluno', description: 'Acadêmico da Instituição' }, 
+    { name: 'Professor', description: 'Professor da Instituição' }, 
+    { name: 'Coordenador', description: 'Coordenador do Curso' }, 
+    { name: 'Diretor', description: 'Diretor de Câmpus' }, 
+    { name: 'Secretário', description: 'Secretário Geral' }]
 
-        document_roles.each do |document_role|
-            DocumentRole.create!(
-            name: document_role[:name],
-            description: document_role[:description],
-        )
-        end
-    end
+  document_roles.each do |document_role|
+    DocumentRole.create!(
+    name: document_role[:name],
+    description: document_role[:description]
+  ) end
+end
   
 #   USED TO CREATE FAKER DOCUMENT ROLES
-    def self.create_with_faker
+  def self.create_with_faker
     DocumentRole.create!(
-      name: Faker::Job.position,
-      description: Faker::Job.title,
-    )
-    end
-
+    name: Faker::Job.position,
+    description: Faker::Job.title,
+  )end
 end
