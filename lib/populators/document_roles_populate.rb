c9d967da161eac64026d4b393958339db54cecde
Lucas Fernando <lucasf.geron@icloud.com>
2023-04-12 15:29:45 -0300

    remove self.populate

--- lib/populators/document_roles_populate.rb
+++ lib/populators/document_roles_populate.rb
@@ -3,12 +3,6 @@ require_relative 'base_populate'
 class Populators::DocumentRolesPopulate < Populators::BasePopulate
   attr_accessor :document_roles
 
-  def self.populate(amount)
-    instance.delete
-    amount.times { instance.create }
-    instance.log(DocumentRole.count)
-  end
-
   def create
     DocumentRole.create!(
       name: Faker::Job.unique.position,
