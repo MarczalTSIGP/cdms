a9dfe105c0832ddbceb71862ec9f4d3020884f58
Lucas Fernando <lucasf.geron@icloud.com>
2023-04-13 13:11:20 -0300

    code quality review

--- test/factories/audience_members.rb
+++ test/factories/audience_members.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :audience_member do
     name { Faker::Name.unique.name }
-    email {  Faker::Internet.unique.email }
+    email { Faker::Internet.unique.email }
     cpf { CPF.generate(true) }
   end
 end
