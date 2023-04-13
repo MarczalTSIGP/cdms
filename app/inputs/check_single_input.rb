8d53006314c4c94ae3810e9e98ab72310623c4a0
Diego Marczal <dmarczal@gmail.com>
2023-04-13 16:29:55 -0300

    Refactored names and added code references

--- app/inputs/check_single_input.rb
+++ app/inputs/check_single_input.rb
@@ -1,3 +1,4 @@
+# From https://tabler.io/preview
 # <div>
 #   <label class="row">
 #     <span class="col">SMS Notifications</span>
@@ -8,15 +9,19 @@
 #     </span>
 #   </label>
 # </div>
-
 class CheckSingleInput < SimpleForm::Inputs::Base
   def input(_wrapper_options)
     template.tag.label(class: 'row') do
-      template.concat span_tag
+      template.concat label_span_tag
       template.concat label_form
     end
   end
 
+  # <span class="col-auto">
+  #   <label class="custom-switch">
+  #     <input class="custom-switch-input" type="checkbox">
+  #   </label>
+  # </span>
   def label_form
     template.tag.span(class: 'col-auto') do
       template.tag.label(class: 'custom-switch') do
@@ -26,7 +31,8 @@ class CheckSingleInput < SimpleForm::Inputs::Base
     end
   end
 
-  def span_tag
+  # <span class="col">SMS Notifications</span>
+  def label_span_tag
     field_name = options[:field_name] ||= object.class.human_attribute_name(attribute_name)
 
     template.tag.span(class: 'col') do
@@ -34,6 +40,8 @@ class CheckSingleInput < SimpleForm::Inputs::Base
     end
   end
 
+  # <span class="custom-switch-indicator"></span>
+  # Show switch single style
   def span_indicator
     template.tag.span(class: 'custom-switch-indicator')
   end
