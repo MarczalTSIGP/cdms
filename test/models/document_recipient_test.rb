6285c427e313a61d1a6058088093084e6efeedaa
Lucas Fernando <lucasf.geron@icloud.com>
2023-04-25 21:50:30 -0300

    tests for this features - before code review(ci)

--- test/models/document_recipient_test.rb
+++ test/models/document_recipient_test.rb
@@ -10,4 +10,31 @@ class DocumentRecipientTest < ActiveSupport::TestCase
     should belong_to(:document)
     should belong_to(:profile)
   end
+
+  context '.from_csv' do
+    should 'call new with args' do
+      document = create(:document)
+      double_service = Struct.new(:perform)
+      tmp_file = Tempfile.new
+      mock = Minitest::Mock.new
+      mock.expect :call, double_service.new(perform: true), [file: tmp_file, document_id: document.id]
+      CreateDocumentRecipientsFromCsv.stub :new, mock do
+        DocumentRecipient.from_csv(tmp_file, document.id)
+      end
+
+      assert_mock mock
+    end
+
+    should 'call perform from instance' do
+      document = create(:document)
+      mock = Minitest::Mock.new
+      mock.expect :perform, true
+      CreateDocumentRecipientsFromCsv.stub :new, mock do
+        DocumentRecipient.from_csv(Tempfile.new, document.id)
+      end
+
+      assert_mock mock
+    end
+  end
+
 end
