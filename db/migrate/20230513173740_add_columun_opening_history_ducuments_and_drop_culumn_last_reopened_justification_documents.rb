bf7fe6d9f71c9a7a08df5154849f1a7ce220b3fc
Diego Marczal <dmarczal@gmail.com>
2023-07-05 14:27:53 -0300

    Changed app timezone and remove unnecessary comments

--- db/migrate/20230513173740_add_columun_opening_history_ducuments_and_drop_culumn_last_reopened_justification_documents.rb
+++ db/migrate/20230513173740_add_columun_opening_history_ducuments_and_drop_culumn_last_reopened_justification_documents.rb
@@ -18,29 +18,3 @@ class AddColumunOpeningHistoryDucumentsAndDropCulumnLastReopenedJustificationDoc
     end
   end
 end
-
-# class AddColumunOpeningHistoryDucumentsAndDropCulumnLastReopenedJustificationDocuments < ActiveRecord::Migration[6.1]
-#   def change
-#     reversible do |dir|
-#       dir.up do
-#         change_table :documents, bulk: true do |t|
-#           t.remove :justification
-#           t.remove :last_reopened_at
-#           t.remove :last_reopened_by_user_id
-#           t.jsonb :opening_history, null: false, default: []
-#           t.index :opening_history, using: :gin
-#         end
-#       end
-
-#       dir.down do
-#         change_table :documents, bulk: true do |t|
-#           t.column :justification, :string
-#           t.column :last_reopened_at, :datetime
-#           t.column :last_reopened_by_user_id, :bigint
-#           t.remove :opening_history, null: false, default: []
-#           t.remove :opening_history, using: :gin
-#         end
-#       end
-#     end
-#   end
-# end
